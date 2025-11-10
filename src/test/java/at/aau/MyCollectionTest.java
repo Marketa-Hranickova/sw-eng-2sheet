9d383cfc4ff2000e630ed969d99eb6b05ee96600
maky <hranickovamarketa@gmail.com>
2025-11-10 11:56:50 +0100

    failed test for empty

--- src/test/java/at/aau/MyCollectionTest.java
+++ src/test/java/at/aau/MyCollectionTest.java
@@ -1,11 +1,47 @@
+
 package at.aau;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class MyCollectionTest {
+
+    private MyCollection collection;
+    private final int CAPACITY = 5;
+
+    @BeforeEach
+    void setUp() {
+        collection = new MyCollection(CAPACITY);
+    }
+
+    /**
+     * should fail
+     */
     @Test
-    void name() {
+    void testEmpty_ClearsCollectionAndResetsSize() {
+        // I am doing this so the collection os not empty
+        collection.add("A");
+        collection.add("B");
+        collection.add("C");
 
+        assertEquals(3, collection.size(),
+                "before testing it should have 3 elements.");
 
+        collection.empty();
+        assertEquals(0, collection.size(),
+                "should be 0.");
+    }
+
+    @Test
+    void testInitialSizeIsZero() {
+        assertEquals(0, collection.size(), "new collection should be 0.");
+    }
+
+    @Test
+    void testSizeIncrementsAfterAdd() {
+        collection.add("TestString");
+        assertEquals(1, collection.size(), "size should increase by 1.");
     }
 }
